5e5a85e758060b29e3b652ae3d6e464289a00d3c
mark-labuschagne <marklabuschagne7@gmail.com>
2022-11-14 11:19:48 +0200

    added integration tests to test registering mediators
    PLAT-598

--- tests/integration.test.js
+++ tests/integration.test.js
@@ -130,3 +130,140 @@ describe("OpenHIM API Authentication", () => {
     );
   });
 });
+
+const testConfig = (filename) => {
+  let mediatorConfig;
+  try {
+    const mediatorConfigFile = fs.readFileSync(resolve("tests", filename));
+
+    mediatorConfig = JSON.parse(mediatorConfigFile.toString());
+  } catch (error) {
+    return error;
+  }
+
+  const openhimConfig = {
+    username: "root@openhim.org",
+    password: "instant101",
+    apiURL: "https://localhost:8080",
+    trustSelfSigned: true,
+    urn: mediatorConfig.urn,
+  };
+
+  return { mediatorConfig, openhimConfig };
+};
+
+describe("Mediator Registration", () => {
+  let { mediatorConfig, openhimConfig } = testConfig("mediatorConfig.json");
+
+  afterEach(async () => {
+    utils.deactivateHeartbeat();
+
+    await DeleteMediator(openhimConfig, mediatorConfig);
+  });
+
+  it("Should return an error when attempting to register a mediator with invalid config", async () => {
+    let { mediatorConfig, openhimConfig } = testConfig(
+      "mediatorConfig-fail.json"
+    );
+
+    let receivedValue;
+    await utils.registerMediator(
+      openhimConfig,
+      mediatorConfig,
+      (returnedValue) => (receivedValue = returnedValue)
+    );
+
+    expect(receivedValue.message).to.eql(
+      "Recieved a non-201 response code, the response body was: Could not add Mediator via the API: ValidationError: name: Path `name` is required."
+    );
+  });
+
+  it("Should return an error from unauthorized activation of the heartbeat", async () => {
+    let { mediatorConfig, openhimConfig } = testConfig("mediatorConfig.json");
+
+    let callbackValue = "";
+    await utils.registerMediator(openhimConfig, mediatorConfig, async () => {
+      await utils.fetchConfig(openhimConfig, async (cVal) => {
+        callbackValue = cVal;
+        openhimConfig.trustSelfSigned = false;
+        const emitter = await utils.activateHeartbeat(openhimConfig);
+        emitter.on("error", (err) => {});
+      });
+    });
+
+    expect(
+      JSON.stringify(callbackValue).includes("DEPTH_ZERO_SELF_SIGNED_CERT")
+    ).to.eql(true);
+    expect(await checkRegistered(openhimConfig, mediatorConfig)).to.eql(null);
+  });
+
+  it("Should return an error from unauthorized activation of the heartbeat", async () => {
+    let { mediatorConfig, openhimConfig } = testConfig("mediatorConfig.json");
+
+    let callbackValue = "";
+    await utils.registerMediator(openhimConfig, mediatorConfig, async () => {
+      openhimConfig.trustSelfSigned = false;
+      await utils.fetchConfig(openhimConfig, async (cVal) => {
+        callbackValue = cVal;
+        const emitter = await utils.activateHeartbeat(openhimConfig);
+        emitter.on("error", (err) => {});
+      });
+    });
+
+    expect(
+      JSON.stringify(callbackValue).includes("DEPTH_ZERO_SELF_SIGNED_CERT")
+    ).to.eql(true);
+  });
+
+  it("Should register a mediator and activate the heartbeat", async () => {
+    let { mediatorConfig, openhimConfig } = testConfig("mediatorConfig.json");
+
+    let callbackValue;
+    await utils.registerMediator(openhimConfig, mediatorConfig, async () => {
+      await utils.fetchConfig(openhimConfig, async (cVal) => {
+        callbackValue = cVal;
+        const emitter = await utils.activateHeartbeat(openhimConfig);
+        emitter.on("error", (err) => {});
+      });
+    });
+
+    expect(callbackValue).to.eql(null);
+    expect(await checkRegistered(openhimConfig, mediatorConfig)).to.eql(null);
+  });
+});
+
+const checkRegistered = async (config, mediatorConfig) => {
+  let headers = {};
+  headers.Authorization =
+    "Basic " +
+    Buffer.from(config.username + ":" + config.password).toString("base64");
+
+  let response;
+  response = await fetch(config.apiURL + "/mediators", {
+    agent: httpsAgent,
+    method: "GET",
+    headers: headers,
+  });
+
+  const respBody = JSON.parse(await response.text());
+
+  expect(respBody.length).to.be.eql(1);
+  expect(respBody[0].name).be.eql(mediatorConfig.name);
+  expect(JSON.stringify(respBody).includes("_lastHeartbeat")).to.eql(true);
+  expect(JSON.stringify(respBody).includes("_uptime")).to.eql(true);
+
+  return null;
+};
+
+const DeleteMediator = async (config, mediatorConfig) => {
+  let headers = {};
+  headers.Authorization =
+    "Basic " +
+    Buffer.from(config.username + ":" + config.password).toString("base64");
+
+  await fetch(config.apiURL + `/mediators/` + mediatorConfig.urn, {
+    agent: httpsAgent,
+    headers,
+    method: "DELETE",
+  });
+};
