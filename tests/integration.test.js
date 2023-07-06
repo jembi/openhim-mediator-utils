369525d4cde7b72d68eaa74722c57ae9f3342a41
bradsawadye <bradsawadye@gmail.com>
2023-07-06 14:16:01 +0200

    Add more tests

--- tests/integration.test.js
+++ tests/integration.test.js
@@ -1,114 +1,100 @@
-/* global describe, it, expect */
+'use strict'
+
+const axios = require('axios');
+const https = require('https');
+const tap = require('tap');
+
 const utils = require('../index.js');
-const request = require('request');
 
-describe('OpenHIM API Authentication', () => {
-  it('Should authenticate and receive headers that allow API access to the OpenHIM', done => {
+tap.test('OpenHIM Authentication, Mediator Registration and Mediator Heartbeat', {autoend: true}, t => {
+  t.test('Should authenticate and receive headers that allow API access to the OpenHIM - with old self cert option', t => {
+    t.plan(1);
+
     utils.authenticate({
       apiURL: 'https://localhost:8080',
       username: 'root@openhim.org',
       rejectUnauthorized: false
-    }, (err, body) => {
-      try {
-        expect(err).toBeNull();
-        expect(body).toBeTruthy();
-        expect(body.salt).toBeTruthy();
-        expect(body.ts).toBeTruthy();
-      } catch (err) {
-        return done(err);
-      }
-      
-      const authHeaders = utils.genAuthHeaders({username: 'root@openhim.org', password: 'openhim-password'});
-    
-      const reqOptions = {
-        url: `https://localhost:8080/channels`,
-        headers: authHeaders,
-        json: true,
-        rejectUnauthorized: false
-      };
-      request.get(reqOptions, (err, res, body) => {
-        try {
-          expect(err).toBeNull();
-          expect(res.statusCode).toBe(200);
-          expect(body).toBeTruthy();
-        } catch (err) {
-          return done(err);
-        }
-
-        done();
-      });
+    }, (_err, body) => {
+      t.ok(body.ts);
     });
   });
 
-  it('Should authenticate and receive headers that allow API access to the OpenHIM - with old self cert option', done => {
-    utils.authenticate({
-      apiURL: 'https://localhost:8080',
-      username: 'root@openhim.org',
-      rejectUnauthorized: false
-    }, (err, body) => {
-      try {
-        expect(err).toBeNull();
-        expect(body).toBeTruthy();
-        expect(body.salt).toBeTruthy();
-        expect(body.ts).toBeTruthy();
-      } catch (err) {
-        return done(err);
-      }
-      
-      const authHeaders = utils.genAuthHeaders({username: 'root@openhim.org', password: 'incorrect'});
-    
-      const reqOptions = {
-        url: `https://localhost:8080/channels`,
-        headers: authHeaders,
-        json: true,
-        rejectUnauthorized: false
-      };
-      request.get(reqOptions, (err, res) => {
-        try {
-          expect(err).toBeNull();
-          expect(res.statusCode).toBe(401);
-        } catch (err) {
-          return done(err);
-        }
+  t.test('Should authenticate and receive headers that allow API access to the OpenHIM - with new self cert option', t => {
+    t.plan(1);
 
-        done();
-      });
-    });
-  });
-
-  it('Should authenticate and receive headers that allow API access to the OpenHIM - with new self cert option', done => {
     utils.authenticate({
       apiURL: 'https://localhost:8080',
       username: 'root@openhim.org',
       trustSelfSigned: true
-    }, (err, body) => {
-      try {
-        expect(err).toBeNull();
-        expect(body).toBeTruthy();
-        expect(body.salt).toBeTruthy();
-        expect(body.ts).toBeTruthy();
-      } catch (err) {
-        return done(err);
-      }
-      
-      const authHeaders = utils.genAuthHeaders({username: 'root@openhim.org', password: 'incorrect'});
-    
-      const reqOptions = {
-        url: `https://localhost:8080/channels`,
-        headers: authHeaders,
-        json: true,
-        rejectUnauthorized: false
-      };
-      request.get(reqOptions, (err, res) => {
-        try {
-          expect(err).toBeNull();
-          expect(res.statusCode).toBe(401);
-        } catch (err) {
-          return done(err);
-        }
-
-        done();
-      });
+    }, (_err, body) => {
+      t.ok(body.ts);
     });
   });
+  
+  t.test('Mediator Registration', {autoend: true}, t => {
+    const testConfig = (filename) => {
+      const mediatorConfig = require(`./${filename}`);
+
+      const openhimConfig = {
+        username: "root@openhim.org",
+        password: "instant101",
+        apiURL: "https://localhost:8080",
+        trustSelfSigned: true,
+        urn: mediatorConfig.urn,
+      };
+    
+      return { mediatorConfig, openhimConfig };
+    }
+
+    const DeleteMediator = async (config, mediatorConfig) => {
+      const headers = {};
+      headers.Authorization =
+        "Basic " +
+        Buffer.from(config.username + ":" + config.password).toString("base64");
+    
+      axios({
+        url: `${config.apiURL}/mediators/${mediatorConfig.urn}`,
+        httpsAgent: new https.Agent({rejectUnauthorized: false}),
+        method: 'DELETE',
+        headers
+      });
+    };
+
+    t.test("Should return an error when attempting to register a mediator with invalid config", t => {
+      t.plan(1);
+
+      let { mediatorConfig, openhimConfig } = testConfig(
+        "mediatorConfig-fail.json"
+      );
+
+      utils.registerMediator(
+        openhimConfig,
+        mediatorConfig,
+        err => {
+          t.equal(err.response.status, 400)
+        }
+      );
+    });
+
+    t.test("Should register a mediator", t => {
+      t.plan(2);
+
+      let { mediatorConfig, openhimConfig } = testConfig(
+        "mediatorConfig.json"
+      );
+
+      utils.registerMediator(
+        openhimConfig,
+        mediatorConfig,
+        err => {
+          t.notOk(err);
+
+          utils.fetchConfig(openhimConfig, (err) => {
+            t.notOk(err);
+            DeleteMediator(openhimConfig, mediatorConfig)
+          })
+        }
+      );
+    });
+  })
 });
